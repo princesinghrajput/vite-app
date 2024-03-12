a6440ab4407780d3c60c78a2e7565ff48f34124d
princesinghrajput <psr8084@gmail.com>
2024-03-12 15:37:59 +0530

    setup react app using vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header'
+
+function App() {
+
+  return (
+    <>
+      <h1>React with Prince</h1>
+      <Header/>
+    </>
+  )
+
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+  return (
+    <div>I am the  Header</div>
+  )
+}
+
+export default Header
